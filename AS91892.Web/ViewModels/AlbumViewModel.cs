3153e23ee40e6bad36c0a4e4452655bfc916e244
Bradley Grover <ac111897@avcol.school.nz>
2022-02-10 11:11:25 +1300

    Add website docs link and album view model

---
+++ AS91892.Web/ViewModels/AlbumViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AS91892.Web.ViewModels;
+
+#nullable disable
+
+/// <summary>
+/// View model to upload the image to the server and store the reference for later use
+/// </summary>
+public class AlbumViewModel : IValidatableObject
+{
+    /// <summary>
+    /// The photo for the album
+    /// </summary>
+    [Required(ErrorMessage = "Please select a file")]
+    [DataType(DataType.Upload)]
+    public IFormFile Photo { get; set; }
+
+    /// <inheritdoc></inheritdoc>
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var photo = ((AlbumViewModel)validationContext.ObjectInstance).Photo;
+        var extension = Path.GetExtension(photo.FileName);
+        var size = photo.Length;
+
+        if (!extension.ToLower().Equals(".jpg"))
+        {
+            yield return new ValidationResult("File extension is not valid.");
+        }
+
+        int maxFileSizeInMegabytes = 10;
+
+        if (size > (maxFileSizeInMegabytes * 1024 * 1024))
+        {
+            yield return new ValidationResult($"File size is bigger than {maxFileSizeInMegabytes}MB.");
+        }
+    }
+}
