d60f244250276349671666c6602c8b4d346a1bb2
Bradley Grover <ac111897@avcol.school.nz>
2022-02-13 22:01:23 +1300

    update tests

---
+++ AS91892.UnitTests/RepoTests/GeneralTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AS91892.Data.Context;
+using AS91892.Data.Entities;
+using AS91892.Data.Repositories;
+using Xunit;
+
+namespace AS91892.Tests.RepoTests;
+
+public class GeneralTests
+{
+    [Fact]
+    public async Task Artist_Linked()
+    {
+        var context = await CreateContext();
+        IArtistRepository repo = new ArtistRepository(context);
+
+        Album album = new() { AlbumCover = null, Id = Guid.NewGuid() };
+
+        Artist artist = new() { ArtistName = "nice", Id = Guid.NewGuid(), Albums = new List<Album>() { album } };
+
+        await repo.CreateAsync(artist);
+
+        Assert.NotNull(await context.Albums.FindAsync(album.Id));
+    }
+    
+    private static async Task<ApplicationDbContext> CreateContext()
+    {
+        var context = Creation.CreateContext((x, r) => r.Artists.Add(x), GetDataset());
+
+        await context.SaveChangesAsync();
+
+        return context;
+    }
+    private static List<Artist> GetDataset() => new()
+        {
+            new Artist() { Id = Guid.NewGuid(), ArtistName = "safsdfklsdfj", Albums = new List<Album>()},
+            new Artist() { Id = Guid.NewGuid(), ArtistName = "somessafjd", Albums = new List<Album>()}
+
+        };
+}
