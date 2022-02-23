e8da570101ff43a4e231bd4143505a3f3bdb5346
Bradley Grover <ac111897@avcol.school.nz>
2022-02-24 09:56:48 +1300

    add assembly info to the lib

---
+++ AS91892.Data/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6bcccc7f-290f-4404-b781-7e6e2d761168")]
+[assembly: InternalsVisibleTo("AS91892.Tests")]
