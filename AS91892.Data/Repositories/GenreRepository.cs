f9bb55171631ef098325a0be194df48c6863ef2e
Bradley Grover <ac111897@avcol.school.nz>
2022-02-24 11:02:58 +1300

    add genre repo

---
+++ AS91892.Data/Repositories/GenreRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace AS91892.Data.Repositories;
+
+
+/// <summary>
+/// Repository for <see cref="GenreRepository"/>
+/// </summary>
+public class GenreRepository : BaseRepository<GenreRepository>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenreRepository"/>
+    /// </summary>
+    /// <param name="context"></param>
+    public GenreRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
