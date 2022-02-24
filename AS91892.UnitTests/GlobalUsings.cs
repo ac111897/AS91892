be723a8dae599d0d8371430e2271e592a6599017
Bradley Grover <ac111897@avcol.school.nz>
2022-02-25 10:11:36 +1300

    update global usings

--- AS91892.UnitTests/GlobalUsings.cs
+++ AS91892.UnitTests/GlobalUsings.cs
@@ -5,4 +5,5 @@ global using System.Collections.Generic;
 global using AS91892.Data.Context;
 global using AS91892.Data.Entities;
 global using AS91892.Data.Repositories;
+global using AS91892.Data.Repositories.Abstractions;
 global using AS91892.Data.ViewModels;
