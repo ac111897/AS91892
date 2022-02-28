082d72cf8561896816c0ee6ed89fa1dce451ebf3
Bradley Grover <ac111897@avcol.school.nz>
2022-02-28 15:09:23 +1300

    add assembly information to the core library

---
+++ AS91892.Core/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("162a3d12-b5ae-43f6-ac1f-d5c1e4ed2114")]
+[assembly: InternalsVisibleTo("AS91892.Tests")]
