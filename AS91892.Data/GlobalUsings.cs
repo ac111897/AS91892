51d44c7121b346a1ffbd98e6bfb3db507a45cc01
Bradley Grover <ac111897@avcol.school.nz>
2022-03-07 14:58:17 +1300

    update data/globalusings: more usings

--- AS91892.Data/GlobalUsings.cs
+++ AS91892.Data/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using System.ComponentModel.DataAnnotations;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
 global using AS91892.Data.Entities;
 global using AS91892.Data.Context;
 global using AS91892.Data.ViewModels;
