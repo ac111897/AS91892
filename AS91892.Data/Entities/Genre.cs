2f72361dc6f6ce19e29a5e83e70c39886076ab4d
Bradley Grover <ac111897@avcol.school.nz>
2022-03-25 09:16:41 +1300

    update validation attributes for genre to disallow empty strings

--- AS91892.Data/Entities/Genre.cs
+++ AS91892.Data/Entities/Genre.cs
@@ -10,7 +10,7 @@ public class Genre : BaseEntity
     /// <summary>
     /// The name of the genre
     /// </summary>
-    [Required]
+    [Required(AllowEmptyStrings = false, ErrorMessage = $"You must provide a value for the {nameof(Genre)}")]
     [StringLength(50, ErrorMessage = $"Length of {nameof(Title)} should be between 2 & 50", MinimumLength = 2)]
     [JsonPropertyName("title")]
     public string Title { get; set; }
